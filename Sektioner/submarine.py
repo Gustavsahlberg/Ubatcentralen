badd00c90f8d520ef8d7b221c5db00c828af9683
Gustav <54705962+Gustavsahlberg@users.noreply.github.com>
2025-09-01 08:06:28 +0200

    Lagt till ny blueprint sida

---
+++ Sektioner/submarine.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request
+from Funktioner.get_files import gather
+
+
+submarine_bp = Blueprint("Ubåt", __name__)
+
+subs = gather.get_sn()
+
+
+@submarine_bp.route("/sök", methods=["GET"])
+def search_resultat():
+    sn = request.args.get("q", "")
+
+    if sn:
+        sn = (f"{sn}.txt")
+        if sn in subs:
+            url = "ubåt.html"
+            sn = (sn[:11])
+        else:
+            url = "error.html"
+        return render_template(f"submarine/{url}",result=sn, sn=sn)
+    
+    return render_template("submarine/search.html", result=None, sn=sn)
