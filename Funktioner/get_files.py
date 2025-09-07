4662ca8dfbaec4ab65dc2c40b5edb8b931c48aa3
Gustav <54705962+Gustavsahlberg@users.noreply.github.com>
2025-09-07 16:35:27 +0200

    funktion som ger dict från givna sn

--- Funktioner/get_files.py
+++ Funktioner/get_files.py
@@ -10,10 +10,14 @@ load = loadReports(folder)
 load.read_files()
 
 
-
+crash_dict = load.get_crash_log()
 
 
 class gather():
     
     def get_sn():
         return set(load.get_sub_sn())
+
+    def get_log(sn):
+        if sn in crash_dict:
+            return crash_dict[sn]
