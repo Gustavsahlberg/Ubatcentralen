09003603afa9e7835b08f11a6994e29a27b238a4
fatmehharrareh95 <f.m.h_2020@hotmail.com>
2025-09-08 11:25:42 +0200

    sensorfel

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from Sektioner.info import info_bp
+from Sektioner.sensors import sensors_bp
 
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config.from_object('config.ConfigDebug')
 
 
 app.register_blueprint(info_bp)
+app.register_blueprint(sensors_bp)
 
 if __name__  == "__main__":
     app.run(debug=True)
