e7c117cd2ca1efff6d98c150bc0cc1c3711cc4f0
Gustav <54705962+Gustavsahlberg@users.noreply.github.com>
2025-09-10 12:28:16 +0200

    Ny funktion som returnar om friendlyfire

--- Funktioner/get_files.py
+++ Funktioner/get_files.py
@@ -21,3 +21,22 @@ class gather():
     def get_log(sn):
         if sn in crash_dict:
             return crash_dict[sn]
+
+    def get_clerance(sn):
+        up, down ,forward = (True,True,True)
+        horizontal, vertical, log = load.get_loc()
+        if sn in log:
+            yx = log[sn]
+            if len(vertical[yx[0]]) > 1:
+                for cordinate in vertical[yx[0]]:
+                    if cordinate > yx[1]:
+                        forward = False
+                        break
+            if len(horizontal[yx[1]]) > 1:
+                for cordinate in horizontal[yx[1]]:
+                    if cordinate > yx[0]:
+                        up = False
+                    elif cordinate < yx[0]:
+                        down = False
+        return up,down,forward
+
