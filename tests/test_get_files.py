5f9a1c4d27bf689f3720063f7c68ccaf12dfba2e
Gustav <54705962+Gustavsahlberg@users.noreply.github.com>
2025-09-17 21:29:21 +0200

    Nytt test för minmax funktionen

--- tests/test_get_files.py
+++ tests/test_get_files.py
@@ -22,3 +22,12 @@ class Testing(unittest.TestCase):
         self.assertIs(up,False)
         self.assertIs(down, True)
         self.assertIs(forward,True)
+
+
+    def test_min_max(self):
+        min_max_list = self.g.min_max()
+
+        self.assertEqual(min_max_list[0][0],"movement_test_4.txt") #max_up
+        self.assertEqual(min_max_list[1][0],"movement_test_1.txt") #min_up
+        self.assertEqual(min_max_list[2][0],"movement_test_0.txt") #max_forward
+        self.assertEqual(min_max_list[3][0],"movement_test_1.txt") #min_forward
