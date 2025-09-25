bb7c22c28bb8bdd05fc9cc1068e6745d3da76d44
Gustav <54705962+Gustavsahlberg@users.noreply.github.com>
2025-09-25 23:26:31 +0200

    Lagt till lite flera tester

--- tests/test_nuke.py
+++ tests/test_nuke.py
@@ -16,16 +16,16 @@ class Testing(unittest.TestCase):
         file = self.folder / f"SecretKEY.txt"
         file.write_text(f"78532111-11:yTjocWZkFj9cCeeJQxfnCyB")
         self.activator = NukeActivator(secret_file="test_nuke/SecretKEY.txt",activation_file="test_nuke/ActivationCodes.txt")
+        self.today = date.today().strftime("%Y-%m-%d")
 
 
 
     def test_nuke_aktivation(self):
-        today = date.today().strftime("%Y-%m-%d")
-        
+    
         submarine_id = "78532111-11"
         secret_key = "yTjocWZkFj9cCeeJQxfnCyB"
         activation_code = "v6wkxY9siR3ZtWueIIAHbx2"
-        user_date = today
+        user_date = self.today
         result = self.activator.activate_nuke(submarine_id, secret_key, activation_code,user_date)
         raw = user_date + secret_key + activation_code
         hash_value = hashlib.sha256(raw.encode()).hexdigest()
@@ -37,7 +37,43 @@ class Testing(unittest.TestCase):
         )
     
 
-    def test_nuke_fail(self):
-        result = self.activator.activate_nuke("1111", "secret_key", "activation_code","user_date")
+
+    def test_wrong_key(self):
+    
+        submarine_id = "78532111-11"
+        secret_key = "WrongKey"
+        activation_code = "v6wkxY9siR3ZtWueIIAHbx2"
+        user_date = self.today
+        result = self.activator.activate_nuke(submarine_id, secret_key, activation_code,user_date)
+
+        self.assertEqual(result, {"status": "error", "message": "ACCESS DENIED"})
+
+    def test_wrong_id(self):
+
+        submarine_id = "WrongID-12"
+        secret_key = "yTjocWZkFj9cCeeJQxfnCyB"
+        activation_code = "v6wkxY9siR3ZtWueIIAHbx2"
+        user_date = self.today
+        result = self.activator.activate_nuke(submarine_id, secret_key, activation_code,user_date)
+
+        self.assertEqual(result, {"status": "error", "message": "ACCESS DENIED"})
+
+    def test_wrong_code(self):
+
+        submarine_id = "78532111-11"
+        secret_key = "yTjocWZkFj9cCeeJQxfnCyB"
+        activation_code = "Wrongcode"
+        user_date = self.today
+        result = self.activator.activate_nuke(submarine_id, secret_key, activation_code,user_date)
+
+        self.assertEqual(result, {"status": "error", "message": "ACCESS DENIED"})
+
+    def test_wrong_day(self):
+
+        submarine_id = "78532111-11"
+        secret_key = "yTjocWZkFj9cCeeJQxfnCyB"
+        activation_code = "v6wkxY9siR3ZtWueIIAHbx2"
+        user_date = "2001-09-25"
+        result = self.activator.activate_nuke(submarine_id, secret_key, activation_code,user_date)
 
         self.assertEqual(result, {"status": "error", "message": "ACCESS DENIED"})
